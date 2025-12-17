fd0a7348961a12d9d6cf3dccdf290dd423fdd320
stasi0xx <stanislaw.korycki.w@gmail.com>
2025-12-17 14:22:25 +0100

    aktualnosci repair

--- app/(public)/aktualnosci/page.tsx
+++ app/(public)/aktualnosci/page.tsx
@@ -39,7 +39,7 @@ export default async function NewsPage() {
                             <Link
                                 key={item.id}
                                 // TUTAJ ZMIANA: Linkujemy do dynamicznego sluga
-                                href={`/app/(public)/aktualnosci/${item.slug}`}
+                                href={`/aktualnosci/${item.slug}`}
                                 className="group bg-white rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300 hover:-translate-y-1 border border-gray-100 flex flex-col h-full"
                             >
                                 <div className="relative h-64 overflow-hidden bg-gray-200">
